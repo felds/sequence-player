21f2b3191ab9c2a52ea5f20222cf7af38f77787b
Luiz “Felds” Liscia <luiz.felds@vetorzero.com.br>
2017-06-20 16:40:34 -0300

    load images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import strExpand from 'str-expand'
+import 'promise-polyfill'
+import 'whatwg-fetch'
 import './sequence-player.css'
+
 
 class SequencePlayer {
     constructor (el, range, options) {
-        this._images = strExpand(range)
         this._el = el
         this._options = Object.assign({}, this.defaultOptions, options)
+
+        this.range = range
 
         this._setupDOM()
     }
@@ -13,7 +17,15 @@ class SequencePlayer {
     get defaultOptions() {
         return {
             aspectRatio: 1,
+            imagesLoadedCallback: undefined,
+            imagesFailedLoadingCallback: undefined,
         }
+    }
+
+    set range(range) {
+        this._range = range
+
+        this._loadImages(strExpand(range))
     }
 
     _setupDOM() {
@@ -24,6 +36,22 @@ class SequencePlayer {
         this._imageContainer.classList.add('sequence-player__image')
         this._el.appendChild(this._imageContainer)
     }
+
+    _loadImages(images) {
+        Promise.all(images.map(f => fetch(f)))
+            .then(success => {
+                console.log('finished loading', typeof this._options.imagesLoadedCallback)
+                if (typeof this._options.imagesLoadedCallback === 'function') {
+                    this._options.imagesLoadedCallback(this)
+                }
+            })
+            .catch(fail => {
+                if (typeof this._options.imagesFailedLoadingCallback === 'function') {
+                    this._options.imagesFailedLoadingCallback(this)
+                }
+            })
+    }
+
 }
 
 window.SequencePlayer = SequencePlayer
