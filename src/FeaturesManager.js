d5910df46327a2e30296c2d951a2ff22404e2912
Luiz “Felds” Liscia <luiz.felds@vetorzero.com.br>
2017-06-22 13:21:10 -0300

    move the anchors

--- src/FeaturesManager.js
+++ src/FeaturesManager.js
@@ -1,3 +1,7 @@
+import { last, head } from 'lodash/fp'
+import { map } from './utils/math'
+
+
 const withAnchors = player => f => {
     const anchor = document.createElement('a')
     anchor.title = f.title
@@ -11,8 +15,8 @@ const withAnchors = player => f => {
     return { ...f, anchor }
 }
 
-const timeToFrame = fps => t => Math.floor(fps * t)
 
+const timeToFrame = fps => t => Math.floor(fps * t)
 const timesToFrames = fps => f => {
     const converter = timeToFrame(fps)
     const waypoints = f.waypoints.map(w => [ converter(w[0]), ...w.slice(1) ])
@@ -42,10 +46,8 @@ export default class FeaturesManager {
 
     set features(fs) {
         this._features = fs
-            .map(withAnchors(this.player))
             .map(timesToFrames(30))
-
-        console.log(this._features)
+            .map(withAnchors(this.player))
     }
     get features() {
         return this._features
@@ -63,6 +65,22 @@ export default class FeaturesManager {
     }
 
     _draw() {
+        const t = this.player.currentFrame
+
+        this.features.forEach(f => {
+            const w_before  = last(f.waypoints.filter(w => w[0] <= t))
+            const w_after   = head(f.waypoints.filter(w => w[0] > t))
+
+            f.anchor.style.opacity = (w_before && w_after && w_before[3]) ? 1 : 0
+
+            if (w_before && w_after) {
+                const amplitude = map(t, w_before[0], w_after[0], 0, 1)
+                f.anchor.style.position = 'absolute'
+                f.anchor.style.left = map(amplitude, 0, 1, w_before[1], w_after[1]) * 100 + "%"
+                f.anchor.style.top = map(amplitude, 0, 1, w_before[2], w_after[2]) * 100 + "%"
+            }
+        })
+
         this._drawToken = requestAnimationFrame(this._draw)
     }
 }
